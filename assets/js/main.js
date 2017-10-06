99961394ac5f5c58c9914972ab3b2f49c67db472
PoudinMR <32468659+PoudinMR@users.noreply.github.com>
2017-10-06 20:01:01 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -99,7 +99,15 @@ jQuery(document).ready(function ($) {
 //    });
 
 
-
+$("#product_iPia").click(function(){
+	$(this).animate({
+    opacity: 0.25,
+    left: "+=5000",
+    height: "toggle"
+  }, 5000, function() {
+    // Animation complete.
+  });
+})
 
     /*---------------------------------------------*
      * WOW
